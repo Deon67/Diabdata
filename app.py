b788c9eaea67bf09eb13c428a37f9681eb61580c
Deon <deonschellito619@gmail.com>
2020-08-22 19:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def main():
-	st.write("# hey it is working")
+	st.write("# hey it is working improvement")
+	st.write("# Good")
     
 if __name__=='__main__':
     main()
